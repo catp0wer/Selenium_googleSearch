2461111ce794b5b598e17ab4ac4fc67dfea4a6d0
catp0wer <catp0wer@mail.ru>
2017-11-12 20:32:38 +0200

    adding comments to the code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,11 +8,15 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Main {
 
     public static void main(String[] args) {
+        //creating and instantiating a Firefox Driver object;
         WebDriver driver = new FirefoxDriver();
+        //opening the browser and navigating to google.com
         driver.get("http://www.google.com");
+        //finding Search field and entering the text
         driver.findElement(By.name("q")).sendKeys("weather in Chisinau");
-
+        //finding Search button and click on it
         driver.findElement(By.name("btnK")).click();
+        //closing the browser
         driver.close();
     }
 }
